0b05357f30dc1ca7d5cf99aeaebb20fa9adc6d9b
riveraacevedor <riveraacevedor@mymacewan.ca>
2025-10-24 04:24:14 -0600

    Update index.html and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import HomePage from "./HomePage";
 import Profile from "./ProfilePage";
 
-
 function App() {
-  return <Profile />;
+  return (
+    <div className="h-full w-full bg-gray-100">
+      <Profile />
+    </div>
+  );
 }
 
 export default App;
